c801e021ec0973aef30d5c04bcde716ad16d5924
Simrit Nijjar <simritnijjar@outlook.com>
2025-04-04 12:59:58 -0700

    added slideshow for images

--- app/ui/components/section/SectionItem.component.tsx
+++ app/ui/components/section/SectionItem.component.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image'
 import {useEffect, useMemo, useState} from 'react'
 
 import CaretDownFilled from '../icons/CaretDownFilled.icon'
+import {IoCloseCircleSharp} from "react-icons/io5";
 import Tag from './Tag.component'
 import { Carousel } from 'antd'
 
@@ -22,13 +23,51 @@ type SectionItemProps = {
 
 export default function SectionItem({className, images, title, description, metadata, url, page, isCompact}: SectionItemProps) {
   const [showMore, setShowMore] = useState(false);
+  const [showCarousel, setShowCarousel] = useState(false);
 
   function toggleShowMore() {
     setShowMore(!showMore);
   }
 
+  const toggleShowCarousel = () => {
+    setShowCarousel(!showCarousel);
+  }
+
+  const imageNodes = useMemo(() => {
+    if (images && images.length > 0) {
+      return images.map((image) => {
+        return <Image
+                key={image}
+                src={image}
+                width={1080}
+                height={192}
+                className={`h-full object-contain scale-90 hover:bg-gray-600 sm:transition-all duration-200 rounded-lg`}
+                alt={`image of ${title} project`}
+              />
+      }
+      )
+    }
+    return null;
+  }, [images, title])
+
   return (
     <>
+
+      <div className={`w-full h-full fixed top-0 left-0 bg-glass dark:bg-black/50 pt-20 
+        backdrop-blur-sm flex flex-col justify-center items-center z-30
+        ${showCarousel ? `block no-doc-scroll` : `hidden`} `}>
+          <button
+            onClick={toggleShowCarousel}
+            className={`absolute text-3xl top-[105] right-10 hover:opacity-50 transition-opacity duration-600 text-white z-[99999]`}
+            title="Close Carousel">
+            <IoCloseCircleSharp />
+          </button>
+          <Carousel key={`carousel-${showCarousel}`} autoplay arrows
+            dots={false} className={`w-[90vw] h-full flex flex-col justify-center items-center`}>
+            {imageNodes}
+          </Carousel>
+        </div>
+     
       <div className={`z-10 flex flex-col justify-between gap-3 bg-glass dark:text-blue-200 text-slate-600 ` + className}
         onClick={toggleShowMore}>
 
@@ -47,20 +86,18 @@ export default function SectionItem({className, images, title, description, meta
 
         <div className={`flex overflow-hidden hover:cursor-pointer gap-5`}>
 
-          <Carousel arrows dotPosition='left'>
-            {images && images.map((image, index) => {
-              console.log(image);
-              return <Image
-                key={index}
-                src={image}
-                width={isCompact ? 200 : 1080}
-                height={isCompact ? 200 : 192}
-                className={`${isCompact ? `h-16 w-16 sm:h-24 sm:w-24 object-cover` : 'object-contain'} hover:scale-110  sm:transition-all duration-200 rounded-lg`}
-                alt={`image of ${title} project`}
-              />
-            })}
-          </Carousel>
-  
+      
+
+          {images && images.length > 0 && (
+            <Image
+              src={images[0]}
+              width={isCompact ? 200 : 1080}
+              height={isCompact ? 200 : 192}
+              className={`${isCompact ? `h-16 w-16 sm:h-24 sm:w-24 object-cover` : 'object-contain'} hover:scale-110 hover:opacity-50 hover:bg-gray-600 sm:transition-all duration-200 rounded-lg`}
+              alt={`image of ${title} project`}
+              onClick={toggleShowCarousel}
+            />
+          )}
 
           {description && isCompact &&
 
