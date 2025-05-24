0614ef05fb2a3332fed21ee09d45a9e8f50f12c3
Simrit Nijjar <simritnijjar@outlook.com>
2025-05-23 23:46:06 -0700

    added missing courses

--- app/ui/pages/Courses.page.tsx
+++ app/ui/pages/Courses.page.tsx
@@ -7,14 +7,14 @@ export default function CoursesPage() {
       <RevealOnScroll>
 
         <Section
-          className={`flex flex-col gap-10 mt-screen`}
-          id={`courses`}
-          header={`Courses`}
-          tagline={`Courses taken at UBC relating to Computer Science and Data Science`}
+          className={'flex flex-col gap-10 mt-screen'}
+          id={'courses'}
+          header={'Courses'}
+          tagline={'Courses taken at UBC relating to Computer Science and Data Science'}
           items={[
             {
               title: "Computation, Programs, and Programming",
-              metadata: ["CPSC 110", "UBC", "2021", "A+"],
+              metadata: ["CPSC 110", "UBC", "2021", "Grade: A+", "Skill: Racket"],
               description: [
                 "Fundamental program and computation structures.",
                 "Introductory programming skills.",
@@ -23,7 +23,7 @@ export default function CoursesPage() {
             },
             {
               title: "Models of Computation",
-              metadata: ["CPSC 121", "UBC", "2022", "B+"],
+              metadata: ["CPSC 121", "UBC", "2022", "Grade: B+", "Skill: Mathematical Proofs"],
               description: [
                 "Physical and mathematical structures of computation.",
                 "Boolean algebra and combinations logic circuits; proof techniques; functions and sequential circuits; sets and relations; finite state machines; sequential instruction execution.",
@@ -31,7 +31,7 @@ export default function CoursesPage() {
             },
             {
               title: "Software Construction",
-              metadata: ["CPSC 210", "UBC", "2022", "A+"],
+              metadata: ["CPSC 210", "UBC", "2022", "Grade: A+", "Skill: Java", "Skill: JUnit"],
               description: [
                 "Design, development, and analysis of robust software components.",
                 "Topics such as software design, computational models, data structures, debugging, and testing.",
@@ -39,7 +39,7 @@ export default function CoursesPage() {
             },
             {
               title: "Introduction to Computer Systems",
-              metadata: ["CPSC 213", "UBC", "2022", "A+"],
+              metadata: ["CPSC 213", "UBC", "2022", "Grade: A+", "Skill: C", "Skill: Assembly"],
               description: [
                 "Software architecture, operating systems, and I/O architectures.",
                 "Relationships between application software, operating systems, and computing hardware; critical sections, deadlock avoidance, and performance; principles and operation of disks and networks.",
@@ -47,35 +47,112 @@ export default function CoursesPage() {
             },
             {
               title: "Basic Algorithms and Data Structures",
-              metadata: ["CPSC 221", "UBC", "2023", "A+"],
+              metadata: ["CPSC 221", "UBC", "2023", "Grade: A+", "Skill: C++"],
               description: [
                 "Design and analysis of basic algorithms and data structures; algorithm analysis methods, searching and sorting algorithms, basic data structures, graphs and concurrency.",
               ]
             },
             {
+              title: "Introduction to Relational Databases",
+              metadata: ["CPSC 304", "UBC", "2023", "Grade: A-", "Skill: SQL"],
+              description: [
+                "Overview of database systems, ER models, logical database design and normalization, formal relational query languages, SQL and other commercial languages,data warehouses, special topics.",
+              ]
+            },
+            {
+              title: "Introduction to Software Engineering",
+              metadata: ["CPSC 310", "UBC", "2023", "Grade: A", "Skill: TypeScript", "Skill: Express.js"],
+              description: [
+                "Specification, design, validation, evolution and construction of modern software systems, within the context of socially and professionally relevant domains such as ethics, intellectual property, and information security.",
+              ]
+            },
+            {
+              title: "Computer Hardware and Operating Systems",
+              metadata: ["CPSC 313", "UBC", "2023", "Grade: A", "Skill: C++"],
+              description: [
+                  "Instruction sets, pipelining, code optimization, caching, virtual memory management, dynamically linked libraries, exception processing, execution time of programs.",
+              ]
+            },
+            {
+              title: "Introduction to Computer Networking",
+              metadata: ["CPSC 317", "UBC", "2024", "Grade: A+", "Skill: C++", "Skill: Java"],
+              description: [
+                  "Computer networking, basic communication protocols, network infrastructure and routing. Common application-level protocols and principles associated with developing distributed applications.",
+              ]
+            },
+            {
+              title: "Intermediate Algorithm Design and Analysis",
+              metadata: ["CPSC 320", "UBC", "2023", "Grade: A-", "Skill: Mathematical Proofs"],
+              description: [
+                  "Systematic study of basic concepts and techniques in the design and analysis of algorithms, illustrated from various problem areas. Topics include: models of computation; choice of data structures; graph-theoretic, algebraic, and text processing algorithms.",
+              ]
+            },
+            {
+              title: "Applied Machine Learning",
+              metadata: ["CPSC 330", "UBC", "2023", "Grade: A+", "Skill: Python"],
+              description: [
+                  "Application of machine learning tools, with an emphasis on solving practical problems. Data cleaning, feature extraction, supervised and unsupervised machine learning, reproducible workflows, and communicating results.",
+              ]
+            },
+            {
+              title: "Machine Learning and Data Mining",
+              metadata: ["CPSC 340", "UBC", "2023", "Grade: A-", "Skill: Python"],
+              description: [
+                  "Models of algorithms for dimensionality reduction, nonlinear regression, classification, clustering and unsupervised learning; applications to computer graphics, computer games, bio-informatics, information retrieval, e-commerce, databases, computer vision and artificial intelligence.",
+              ]
+            },
+            {
+              title: "Distributed Systems",
+              metadata: ["CPSC 416", "UBC", "2024", "Grade: B+", "Skill: GoLang"],
+              description: [
+                  "Concepts and design of distributed systems. Communication architecture and models for interprocess communication. Process migration, naming, distributed file systems, fault tolerance, and concurrency control.",
+              ]
+            },
+            {
+              title: "Computer Vision",
+              metadata: ["CPSC 425", "UBC", "2024", "Grade: A", "Skill: Python"],
+              description: [
+                  "Introduction to the processing and interpretation of images. Image sensing, sampling, and filtering. Algorithms for colour analysis, texture description, stereo imaging, motion interpretation, 3D shape recovery, and recognition.",
+              ]
+            },
+            {
+              title: "Video Game Programming",
+              metadata: ["CPSC 427", "UBC", "2024", "Grade: A", "Skill: C++", "Skill: OpenGL"],
+              description: [
+                  "Video game programming techniques and technologies, including rendering, animation, interaction, game AI, real-time software development for games; start-to-finish simple video-game design and implementation.",
+              ]
+            },
+            {
+              title: "Computer Security",
+              metadata: ["CPSC 436S", "UBC", "2024", "Grade: A", "Skill: Python"],
+              description: [
+                  "This wide-ranging topics course covers fundamental concepts in computer security and privacy.",
+              ]
+            },
+            {
               title: "Differential Calculus with Applications",
-              metadata: ["MATH 100", "UBC", "2021", "B"],
+              metadata: ["MATH 100", "UBC", "2021", "Grade: B"],
               description: [
                 "Derivatives of elementary functions. Applications and modelling: graphing, optimization.",
               ]
             },
             {
               title: "Integral Calculus with Applications",
-              metadata: ["MATH 101", "UBC", "2022", "A-"],
+              metadata: ["MATH 101", "UBC", "2022", "Grade: A-"],
               description: [
                 "The definite integral, integration techniques, applications, modelling, infinite series.",
               ]
             },
             {
               title: "Calculus III",
-              metadata: ["MATH 200", "UBC", "2022", "A+"],
+              metadata: ["MATH 200", "UBC", "2022", "Grade: A+"],
               description: [
                 "Analytic geometry in 2 and 3 dimensions, partial and directional derivatives, chain rule, maxima and minima, second derivative test, Lagrange multipliers, multiple integrals with applications.",
               ]
             },
             {
               title: "Matrix Algebra",
-              metadata: ["MATH 221", "UBC", "2023", "A"],
+              metadata: ["MATH 221", "UBC", "2023", "Grade: A"],
               description: [
                 "Systems of linear equations, operations on matrices, determinants, eigenvalues and eigenvectors, diagonalization of symmetric matrices",
               ]
@@ -83,7 +160,7 @@ export default function CoursesPage() {
           ]}
         />
 
-        <h1 className='text-center pt-5 opacity-70 text-sm'> All course names & descriptions are originally from <a target='_blank' href='https://courses.students.ubc.ca'> courses.students.ubc.ca</a>  as of 2023/09/20 </h1>
+        <h1 className='text-center pt-5 opacity-70 text-sm'> All course names & descriptions are originally from <a target='_blank' rel="noreferrer" href='https://courses.students.ubc.ca'> courses.students.ubc.ca</a>  as of 2023/09/20 </h1>
 
       </RevealOnScroll>
     </>
